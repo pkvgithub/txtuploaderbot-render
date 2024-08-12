87c278bacff4caff325eb46d2a0870971ff76045
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-08-12 20:31:47 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
+API_ID    = os.environ.get("API_ID", "25129557")
+API_HASH  = os.environ.get("API_HASH", "83c9546cfdee154fd2d16759c4d0582a")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 WEBHOOK = True  # Don't change this
